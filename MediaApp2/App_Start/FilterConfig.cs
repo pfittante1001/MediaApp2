d2a31f0263faca025d6e6db8071009b96e7c2879
pfittante <pfittante@yahoo.com>
2017-07-25 16:15:26 -0400

    inital commit

---
+++ MediaApp2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MediaApp2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
